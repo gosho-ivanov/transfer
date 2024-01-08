24666c9062fbefdd7b4d938fff2e5d981b74ee31
ThanatosZMD <georgizeemod@gmail.com>
2024-01-08 14:08:01 +0200

    Finalized the backpack program

--- Intro to algorythms and data structures/backpack.py
+++ Intro to algorythms and data structures/backpack.py
@@ -8,7 +8,7 @@ statistics = []
 for i in range(n):
     items_weight.append(int(input("Enter the weight of the current item (between 2 and 9 kgs): ")))
 
-permutations_items = permutations(items_weight)
+permutations_items = list(permutations(items_weight))
 
 for perm in permutations_items:
     m = backpack
@@ -23,15 +23,25 @@ for perm in permutations_items:
     statistics.append(data)
 
 filled_backpack = []
-
+perms_locations = []
 for variant in statistics:
     total = 0
     for key in variant.keys():
         total += key*variant[key]
         if total == backpack:
-            filled_backpack.append(variant)
+            if variant not in filled_backpack:
+                filled_backpack.append(variant)
+                perms_locations.append(statistics.index(variant))
+            else:
+                filled_backpack.append(variant)
+                perms_locations.append(statistics.index(variant)+1)
 
+index = 0
 for variant in filled_backpack:
-    print(f"\nThe following variant fills the backpack succesfully:")
-    for key in variant:
+    print(f"\nThe following variant fills the backpack succesfully:\n"
+          f"Permutation: {permutations_items[perms_locations[index]]}")
+    for key in filled_backpack[index].keys():
         print(f"{key} kg item: {variant[key]} times")
+    index+=1
+
+print(f"\n{len(permutations_items)} permutations were created and only {len(filled_backpack)} filled the backpack succesfully.")

9950a7286d48e1bdba53efa6060ee1a9560dbdf9
ThanatosZMD <georgizeemod@gmail.com>
2024-02-27 22:58:33 +0200

    Homework for MODERN TECHNOLOGIES

---
+++ MODERN TECHNOLOGIES/Homeworks/06-students-tests.py
@@ -0,0 +1,15 @@
+from math import ceil
+
+class_grades = {}
+student_id = 1
+points = input()
+while points != "":
+    points = list(map(int, points.split(", ")))
+    class_grades[student_id] = points
+    student_id += 1
+    points = input()
+
+for student, points in class_grades.items():
+    avg_score = ceil(sum(points) / len(points))
+    grade = 2 + (avg_score/100 * 4)
+    print(f"{student}: {avg_score} -> {grade:.2f}")

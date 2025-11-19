14c90fb53b33643dc667e4c1834b0212ca748fce
gosho-ivanov <georgizeemod@gmail.com>
2025-11-19 20:55:49 +0200

    shami

--- Intro to OOP/Intro to OOP/src/Shami/PresentWeb/Main.java
+++ Intro to OOP/Intro to OOP/src/Shami/PresentWeb/Main.java
@@ -1,4 +1,27 @@
 package Shami.PresentWeb;
 
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Present[] presents = new Present[10];
+        Present present = null;
+        for (int i= 0; i < 10; i++) {
+            int id = scanner.nextInt();
+            double price = scanner.nextDouble();
+            String type = scanner.nextLine();
+            String size = scanner.nextLine();
+            present = new Present(id, price, size, type);
+            presents[i] = present;
+        }
+
+        Present present1 = Present.getPresentByID(presents, 1);
+        Present present2 = Present.getPresentByType(presents, "clothing");
+        if (present1 != null) System.out.println(present1.toString());
+        if (present2 != null) System.out.println(present2.toString());
+    }
+
+
 }
--- Intro to OOP/Intro to OOP/src/Shami/PresentWeb/Present.java
+++ Intro to OOP/Intro to OOP/src/Shami/PresentWeb/Present.java
@@ -1,4 +1,75 @@
 package Shami.PresentWeb;
 
 public class Present {
+    private int id;
+    private double price;
+    private String type;
+    private String size;
+
+    public Present(int id, double price, String size, String type) {
+        this.id = id;
+        this.price = price;
+        this.size = size;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Present{" +
+                "id=" + id +
+                ", price=" + price +
+                ", type='" + type + "'" +
+                ", size='" + size + "'" +
+                '}';
+    }
+
+    public static Present getPresentByID(Present[] presents, int presentId){
+        for (Present present : presents){
+            if (present.getId() == presentId){
+                return present;
+            }
+        }
+        return null;
+    }
+
+    public static Present getPresentByType(Present[] presents, String presentType){
+        for (Present present : presents){
+            if (present.getType().equals(presentType)){
+                return present;
+            }
+        }
+        return null;
+    }
 }
